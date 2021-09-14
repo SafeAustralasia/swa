2db7a72f93f7b4e56c7fa0ddb8727dc6052d6dcc
AlanSMat <steve@asmather.com>
2021-09-14 18:33:58 +1000

    slight change to footer

--- footer.php
+++ footer.php
@@ -6,24 +6,25 @@
  */
 ?>
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4">
-                    <h5>Icons</h5>
-                    <?php get_template_part('template-parts/icons/social-media','grey'); ?>    
-                </div>
-                <div class="col-lg-4">
-                    <h5>Quick Links</h5>
-                    <?php get_template_part('template-parts/nav/nav','footer'); ?>    
-                </div>
-                <div class="col-lg-4">
-                    <h5>Contact Info</h5>
-                    <a href="#">info@safeworkaustralia.org.au</a>
-                </div>
-            </div>
-        </div>
-    </footer>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <h5>Icons</h5>
+        <?php get_template_part('template-parts/icons/social-media','grey'); ?>
+      </div>
+      <div class="col-lg-4">
+        <h5>Quick Links</h5>
+        <?php get_template_part('template-parts/nav/nav','footer'); ?>
+      </div>
+      <div class="col-lg-4">
+        <h5>Contact Info</h5>
+        <a href="#"></a>
+      </div>
+    </div>
+  </div>
+</footer>
 <?php wp_footer() ?>
 </body>
+
 </html>
