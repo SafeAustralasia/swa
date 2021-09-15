06a33a559e722835aea4d8a1c8193074c5244aca
AlanSMat <steve@asmather.com>
2021-09-15 10:12:34 +1000

    fixed font problem

--- template-parts/header/header-static-image.php
+++ template-parts/header/header-static-image.php
@@ -2,12 +2,9 @@
 
 </style>
 <div class="static-image-container overlay-black-200">
-    <img src="<?php echo ASM_ASSETS_URI ?>/images/banner/asparagus-meat.jpg" alt="woman hat" class="static-image">
-	<div class="header-text">
-		<h1>Heading Text</h1>
-		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, nobis laborum? Molestias minus, velit dolor magni tempore saepe ut sed?</p>
-	</div>
+  <img src="<?php echo ASM_ASSETS_URI ?>/images/banner/asparagus-meat.jpg" alt="woman hat" class="static-image">
+  <div class="header-text">
+    <h1>Low Carb Lifestyle</h1>
+    <p>Espousing the benefits of a low carb lifestyle</p>
+  </div>
 </div>
-
-
-
