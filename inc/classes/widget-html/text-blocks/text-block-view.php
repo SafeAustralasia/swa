5f848e110917324f92d8d4c6306fa3a4ad3d97f7
AlanSMat <steve@asmather.com>
2021-09-16 10:31:24 +1000

    changes to blog code

--- inc/classes/widget-html/text-blocks/text-block-view.php
+++ inc/classes/widget-html/text-blocks/text-block-view.php
@@ -1,13 +1,14 @@
 <div class="container home-text-block-container" id="text-block-<?php echo $this->id ?>">
-    <div class="row">
-        <div class="col-lg-6">
-            <img src="<?php echo $instance['imageuri'] ?>" alt="">
-        </div>
-        <div class="col-lg-6 text-block">
-            <h1><?php echo $instance['title']; ?></h1>
-            <p><?php echo nl2br($instance['details']); ?></p>
-            <p><a href="<?php echo $instance['linkuri']; ?>" class="btn btn-secondary"><?php echo $instance['linktext']; ?></a></p>
-        </div>
-    </div>
-</div>
+  <div class="row">
+    <div class="col-lg-6"
+      style="background-image: url(<?php echo $instance['imageuri'] ?>); height: 25em; background-position: center; background-repeat: no-repeat; background-size: cover;">
 
+    </div>
+    <div class="col-lg-6 text-block">
+      <h1><?php echo $instance['title']; ?></h1>
+      <p><?php echo nl2br($instance['details']); ?></p>
+      <p><a href="<?php echo $instance['linkuri']; ?>"
+          class="btn btn-secondary"><?php echo $instance['linktext']; ?></a></p>
+    </div>
+  </div>
+</div>
--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -1,17 +1,17 @@
 <main id="main" class="page-main" role="main">
   <?php if( have_posts() ) :  ?>
-  <div class="container">
+  <div class="container blog-excerpt-container">
     <?php while ( have_posts() ) : the_post(); ?>
-    <article class="row" id="post-<?php the_ID(); ?>">
-      <div class="col">
+    <div class="row mb-4" id="post-<?php the_ID(); ?>">
+      <div class="col-lg-4">
         <img src="<?php the_post_thumbnail_url() ?>" alt="<?php the_title() ?>" class="img-fluid">
       </div>
-      <div class="col">
+      <div class="col-lg-8">
         <h3><?php the_title() ?></h3>
         <?php the_excerpt() ?>
         <a href="<?php the_permalink(); ?>" class="btn btn-sml btn-primary">Read more</a>
       </div>
-    </article>
+    </div>
     <?php endwhile ?>
   </div> <!-- end container -->
   <?php endif  ?>
