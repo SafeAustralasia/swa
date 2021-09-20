0b4a6676079dfe68e6290a4701385a1fce40e3f5
AlanSMat <steve@asmather.com>
2021-09-20 13:49:45 +1000

    changes to stylibng for static image header

--- template-parts/header/header-page.php
+++ template-parts/header/header-page.php
@@ -1,5 +1,11 @@
-<div class="container-fluid content-page-header">
-  <div class="content-page-title">
-    <?php is_home() && ! is_front_page() ? single_post_title() : the_title() ?>
+<style>
+
+</style>
+<div class="static-image-container overlay-black-600 height-20em">
+  <?php if( has_post_thumbnail() ) : ?>
+    <?php the_post_thumbnail( 'full' ); ?>
+  <?php endif ?>
+  <div class="page-header-title">
+    <h1><?php is_home() && ! is_front_page() ? single_post_title() : the_title() ?></h1>
   </div>
 </div>
--- template-parts/header/header-static-image.php
+++ template-parts/header/header-static-image.php
@@ -1,7 +1,7 @@
 <style>
 
 </style>
-<div class="static-image-container overlay-black-200">
+<div class="static-image-container overlay-black-400 height-50em">
   <img src="<?php echo ASM_ASSETS_URI ?>/images/banner/asparagus-meat.jpg" alt="woman hat" class="static-image">
   <div class="header-text">
     <h1>Low Carb Lifestyle</h1>
