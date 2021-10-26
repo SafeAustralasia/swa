e318cba21c771da96358a600181f81add3de8666
AlanSMat <steve@asmather.com>
2021-10-26 12:22:05 +1100

    added formatting on front page and for block patterns

--- front-page.php
+++ front-page.php
@@ -10,29 +10,33 @@ get_header();
 <div class="height-30em strip-bg"></div> 
 <div class="container about-us-container">
     <div class="row">
-        <div class="col-md-6 col-sm-12 about-us-text">
-            <!-- wp:heading -->
-            <h2 class="sub-heading">About Us</h2>
-            <!-- /wp:heading -->
+        <div class="col-md-6 col-sm-12">
+            <div class="about-us-card about-us-text">
+                <!-- wp:heading -->
+                <h2 class="sub-heading">About Us</h2>
+                <!-- /wp:heading -->
 
-            <!-- wp:paragraph -->
-            <p>The SafeAustralasia Foundation is a not-for-profit registered charity based in Australia seeking to support a number of initiatives as per our goals.</p>
-            <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p>The SafeAustralasia Foundation is a not-for-profit registered charity based in Australia seeking to support a number of initiatives as per our goals.</p>
+                <!-- /wp:paragraph -->
 
-            <!-- wp:paragraph -->
-            <p>We have established in our region to provide a similar framework and efforts based on the outstanding work being carried out by the SafeAmerica Foundation, our sister organisation, who have been operating for over 25 years in the USA in a similar capacity. <em>The SafeAmerica Foundation </em><em>organisation was founded in 1994 and partners with corporate, government, public and private sector organizations and non-profit organization to improve the awareness of safety and preparedness in the United States.&nbsp;</em></p>
-            <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p>We have established in our region to provide a similar framework and efforts based on the outstanding work being carried out by the SafeAmerica Foundation, our sister organisation, who have been operating for over 25 years in the USA in a similar capacity. <em>The SafeAmerica Foundation </em><em>organisation was founded in 1994 and partners with corporate, government, public and private sector organizations and non-profit organization to improve the awareness of safety and preparedness in the United States.&nbsp;</em></p>
+                <!-- /wp:paragraph -->
 
-            <!-- wp:paragraph -->
-            <p>The SafeAustralasia Foundation see ourselves as a sister organisation carrying out equivalent programs relevant and appropriate to the Australasian region.</p>
-            <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p>The SafeAustralasia Foundation see ourselves as a sister organisation carrying out equivalent programs relevant and appropriate to the Australasian region.</p>
+                <!-- /wp:paragraph -->
 
-            <!-- wp:paragraph -->
-            <p>For more information about the SafeAmerica Foundation see <a target="blank" href="https://safeamerica.org/"><em>www.safeamerica.org</em></a></p>
-            <!-- /wp:paragraph -->
+                <!-- wp:paragraph -->
+                <p>For more information about the SafeAmerica Foundation see <a target="blank" href="https://safeamerica.org/"><em>www.safeamerica.org</em></a></p>
+                <!-- /wp:paragraph -->
+            </div>
         </div>
         <div class="col-md-6 col-sm-12">
-            <img src="<?php echo ASM_BUILD_IMG_URI ?>/Harold Len and Sec Norm Manetta.jpg"/>
+            <div class="about-us-card">
+                <img src="<?php echo ASM_BUILD_IMG_URI ?>/Harold Len and Sec Norm Manetta.jpg"/>
+            </div>
         </div>
     </div>
 </div>
@@ -60,6 +64,18 @@ get_header();
                 <li>Assisting Emergency Responders skill development</li>
             </ul>
             <p>All courses sponsored are to be Australian educational institution in approved Australian courses, and/or at educational institutions overseas to study as a component of an approved Australian course</p>
+            <ul>
+                <li>
+                    Build our membership and engage with organisations in the corporate, government, public and private sector through the:
+                </li>
+                <ul>
+                    <li>Safety News Network</li>
+                    <li>WorldSafe CEO Network</li>
+                    <li>And other initiatives to communicate to a wide audience</li>
+                </ul>
+                <li>Involve the ‘next generation’ as proactive leaders, and</li>
+                <li>Develop sustainable Australasian programs in other related safety subject across our region</li>
+            </ul>
         </div>
     </div>
 </div>
